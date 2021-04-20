7eaa030a965b8141f2aebcd5dc76ed8b72e03b7c
Kelsie <kelsie51@hotmail.com>
2021-04-20 11:22:55 -0500

    set up model folder

---
+++ models/Blog.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    blog_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    blog_content: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    timestamp: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Blog;

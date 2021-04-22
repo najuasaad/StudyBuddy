f3335d05f137fc2aa39549f698ba7b944cd130a4
Najua Saad <najuasaad@gmail.com>
2021-04-22 11:14:23 -0500

    seeds

---
+++ seeds/session_members.js
@@ -0,0 +1,42 @@
+[
+    {
+        "member_id": 1,
+        "session_id": 5,
+    },
+    {
+        "member_id": 1,
+        "session_id": 4,
+    },
+    {
+        "member_id": 1,
+        "session_id": 2,
+    },
+    {
+        "member_id": 2,
+        "session_id": 1,
+    },
+    {
+        "member_id": 3,
+        "session_id": 5,
+    },
+    {
+        "member_id": 3,
+        "session_id": 2,
+    },
+    {
+        "member_id": 3,
+        "session_id": 1,
+    },
+    {
+        "member_id": 4,
+        "session_id": 5,
+    },
+    {
+        "member_id": 5,
+        "session_id": 1,
+    },
+    {
+        "member_id": 5,
+        "session_id": 2,
+    },
+]

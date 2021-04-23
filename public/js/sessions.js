2c9c429f78c586de9699ab50fe79978c479687c2
Eric Heltemes <e.heltemes83@gmail.com>
2021-04-23 00:12:38 -0500

    if user is logged in they can now enroll from main sessions page

--- public/js/sessions.js
+++ public/js/sessions.js
@@ -1,32 +1,39 @@
 async function enrollSession(event) {
-    event.preventDefault();
-    // using session user info to add them to the attendee id list if the list is not full
-    const member_id = req.session.user_id
-    const session_id = event.target.getAttribute('data-id');
-    
-    if (member_id && session_id) {
-        const response = await fetch(`/`, {
-            method: 'POST',
-            body: JSON.stringify({
-                member_id,
-                session_id,
-            })
-        }) 
+    console.log("element selected")
+    if (event.target.hasAttribute('data-id')) {
+        event.preventDefault();
+        console.log("button and user passed")
+        // using session user info to add them to the attendee id list if the list is not full
+            // const member_id = req.session.user_id
+            const session_id = event.target.getAttribute('data-id');
+            console.log(session_id)
+            
+            if (session_id) {
+                const response = await fetch(`/api/sessionmembers/`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        session_id,
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }) 
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-          } else {
-            alert('Failed to enroll.');
-        }
-    } else {
-        // redirect to login
+                if (response.ok) {
+                    document.location.replace('/');
+                } else {
+                    alert('Failed to enroll.');
+                }
+            } else {
+                // redirect to login
+            }
+
     }
- 
 }
 
-async function goToNewSessionPage(event) {
+// async function goToNewSessionPage(event) {
     // redirect to add session route
-}
+// }
 
-document.querySelector('.enroll').addEventListener('submit', enrollSession);
-document.querySelector('#addNewSession').addEventListener('submit', goToNewSessionPage)
+document.querySelector('#sessionListener').addEventListener('click', enrollSession);
+// document.querySelector('#addNewSession').addEventListener('click', goToNewSessionPage)

e1c9f3366dfdc96d4b30ef5ffd5b4f01f5808203
Eric Heltemes <e.heltemes83@gmail.com>
2021-04-23 19:03:14 -0500

    radio image select in signup functional

--- public/js/signup.js
+++ public/js/signup.js
@@ -17,11 +17,18 @@ async function newFormUser(event) {
   const title = document.querySelector('#inputTitle1').value.trim();
   const city = document.querySelector('#inputCity').value.trim();
   const state = document.querySelector('#inputState').value.trim();
-  const profilePicture = document.getElementsByName('profilePicture').value;
+  const profilePicture =  getProfilePicture()
 
-  // if ( isValidEmail(email) !== true ) {
-  //   prompt('Must use a valid email')
-  // }
+  function getProfilePicture() {
+    const rd1 = document.getElementById('catProfilePicture')
+    const rd2 = document.getElementById('garyProfilePicture')
+  // const rd3 = document.getElementId('ProfilePicture')
+  // const rd4 = document.getElementId('ProfilePicture')
+    if (rd1.checked === true){return rd1.value} 
+    else if (rd2.checked === true){return rd2.value}
+  // if (rd3.checked===true){rd3.value = profilepicture}
+  // if (rd4.checked===true){rd4.value = profilepicture}
+  }
   
   if (email && password && display_name && title) {
     console.log("if statement passed")
--- seeds/seed.js
+++ seeds/seed.js
@@ -5,13 +5,10 @@ const notesSeedData = require('./notes_seed.json');
 const membersSeedData = require('./members_seed.json');
 const sessionSeedData = require('./sessions_seed.json');
 const sessionMemberSeedData = require('./session_members.json');
-const imagesSeedData = require('./images_seed.json')
 
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
-  await Images.bulkCreate(imagesSeedData);
 
   await Members.bulkCreate(membersSeedData);
 
@@ -20,7 +17,6 @@ const seedDatabase = async () => {
   await Sessions.bulkCreate(sessionSeedData);
 
   await SessionMember.bulkCreate(sessionMemberSeedData)
-
 
   process.exit(0);
 };
